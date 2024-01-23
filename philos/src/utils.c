b2dc74698bdab85ba9892f0c04b81fa5026bd99c
Brayan Saiago <90729742+brayans22@users.noreply.github.com>
2024-01-23 23:48:27 +0100

    Update utils.c

--- philos/src/utils.c
+++ philos/src/utils.c
@@ -27,30 +27,6 @@ void	ft_putstr_fd(char *s, int fd)
 	s = first;
 }
 
-int	ft_atoi(const char *str)
-{
-	int	sign;
-	int	nb;
-
-	sign = 1;
-	nb = 0;
-	while (*str == '\t' || *str == '\v' || *str == '\f' || \
-		*str == '\n' || *str == ' ' || *str == '\r')
-		str++;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign = -sign;
-		str++;
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		nb = nb * 10 + *str - '0';
-		str++;
-	}
-	return (nb * sign);
-}
-
 int	ft_strlen(const char *s1)
 {
 	int	i;
